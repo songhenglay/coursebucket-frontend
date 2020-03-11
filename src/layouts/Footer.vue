9d4e07de37c5db930d0e1c18e7b749ca12860e6a
Songheng Lay <songhenglay16@gmail.com>
2020-03-11 17:10:49 +0700

    update

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer padless absolute>
+        <v-card flat tile width="100%" class="blue lighten-1 text-center">
+            <v-card-text class="white--text font-weight-bold">Made with love by 353 Technologies</v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/layouts/TopicCards.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-container class="programming">
+            <v-card class="ma-3" width="170" v-for="(course,i) in courses" :key="i">
+                <v-img height="150" :src="course.img"></v-img>
+                <v-card-title class="body-2">{{course.name}}</v-card-title>
+                <v-card-subtitle class="caption pb-4">{{course.role}}</v-card-subtitle>
+            </v-card>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopicCards',
+    data(){
+        return{
+            course:[
+                { name:'Python', role:'Programming'},
+            ]
+        }
+    }
+}
+</script>
